c2317bf21f933b27fdc1c1891a47d6027f54511b
DeStRaaM <20destraam03@gmail.com>
2022-08-09 21:17:47 +0200

    Final commit. Added cart functionality.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Item from './components/Item';
 import Topbar from './components/Topbar';
-import data from "./Database.js"
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -10,56 +9,85 @@ import Cartitem from "./components/Cartitem.js"
 
 
 
-
 const App = () => {
 
   const [cartArrayData, setCartArrayData] = useState([]);
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState([])
+  var length = cartArrayData.length;
+  var count = 1
 
-  const createCartItem = (cartArrayData) => {
-    return(
-    <Cartitem 
-      id={cartArrayData.id}
-      title={cartArrayData.title}
-      price={cartArrayData.price}
-      image={cartArrayData.image}
-     />)
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/')
+            .then(res=>res.json())
+            .then(json=>{
+              setData(json.map((i)=>{return(i)})) 
+            })
+          },[])
+
+  const onDelete = (id) => {
+      setCartArrayData(prevValue => {
+        return(
+          prevValue.filter(
+            (item, index)=>{
+              return(
+                cartArrayData[index].id !== id
+              )
+              }
+            )
+          )
+        }
+      )
   }
 
   const onAdd = (clickedItem) => {
     var a = []
     a = data.filter(element => element.id == clickedItem)
-    setCartArrayData(prevValue => [...prevValue, a[0]])
-  }
-
-  const createItem = (data) => {
-    return(
-      <Item
-        id={data.id}
-        title = {data.title}
-        image = {data.image}
-        price = {data.price}
-        descr = {data.description}
-        onAdd = {onAdd}
-       />
-    )
+    cartArrayData.findIndex((i)=>i.id === a[0].id) === -1 ?
+     setCartArrayData(prevValue => [...prevValue, a[0]]) : 
+     setCartArrayData(prevValue => [...prevValue])
   }
 
   return (
     <div className="App bg-[#FFDEDE]">
       <Topbar />
       <div className=" absolute right-5 top-5">
-        <div>
-        <Button onClick={() => setOpen(true)}><ShoppingCartIcon /></Button>
-        <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
-          <h3 className=" text-3xl m-5 mr-28">Your Shopping Cart</h3>
-          {cartArrayData.map(createCartItem)}
-        </Drawer>
-    </div>
+        <div className=''> 
+          <a className=' text-2xl'>{length}</a>
+          <Button className=' relative bottom-1' onClick={() => setOpen(true)}><ShoppingCartIcon /></Button>
+          <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
+            <h3 className=" text-3xl m-5 mr-28">Your Shopping Cart</h3>
+            {cartArrayData.map((cartArrayData) => {
+              return(
+                <Cartitem 
+                  id={cartArrayData.id}
+                  title={cartArrayData.title}
+                  price={cartArrayData.price}
+                  image={cartArrayData.image}
+                  onDelete={onDelete}
+                  count={count}
+                />)
+                }
+              )
+            }
+          </Drawer>
+        </div>
       </div>
-      <div className='flex flex-wrap'>
-        {data.map(createItem)}
-      </div>
+        <div className='flex flex-wrap'>
+          {data.map((data)=>{
+            return(
+              <Item
+                id={data.id}
+                title = {data.title}
+                image = {data.image}
+                price = {data.price}
+                descr = {data.description}
+                onAdd = {onAdd}
+              />)
+              }
+            )
+          }
+        </div>
     </div>
   );
 }
--- src/components/Cartitem.js
+++ src/components/Cartitem.js
@@ -1,19 +1,40 @@
 import { Button } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import ButtonGroup from "@mui/material/ButtonGroup";
+import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+import { useState } from "react";
 
 const Cartitem = (props) => {
+
+    const [count, setCount] = useState(1)
+
+    const handleDelete = () => {
+            props.onDelete(props.id)
+    }
+
+    const handleAdd = () => {
+            setCount(count+1)
+    }
+
+    const handleMinus = () => {
+       count > 1 ? setCount(count-1) :  props.onDelete(props.id)
+    }
 
     return(
         <div className=" grid-flow-col grid w-[30rem] rounded-lg mr-5 ml-5 mb-5 bg-slate-400">
             <div className=" text-center">
                 <p className=" font-bold text-2xl h-20 w-64 mt-2 ml-5">{props.title}</p>
-                <p className=" text-xl m-10 text-center font-semibold">{props.price}$</p>
-                <div className=" grid grid-flow-col mt-28 ml-3 mb-5 bg-slate-100 rounded-md">
-                    <Button className=" w-20" variant="text"><RemoveIcon /></Button>
-                    <a className=" m-auto pl-10 pr-10">count</a>
-                    <Button className=" w-20" variant="text"><AddIcon /></Button>
+                <p className=" text-xl m-10 mt-12 ml-14 text-center font-semibold">Total: {Math.round(count * props.price*100)/100}$</p>
+                <div className=" grid ml-24 mr-20 mb-5">
+                    <a className="m-auto text-lg ">{count}</a>
                 </div>
+                    <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                        <Button onClick={handleMinus}><RemoveIcon /></Button>
+                        <Button onClick={handleDelete}><DisabledByDefaultIcon /></Button>
+                        <Button onClick={handleAdd}><AddIcon /></Button>
+                    </ButtonGroup>
+                
             </div>
             <img className=" object-cover p-5 h-80 " 
             src={props.image} />
